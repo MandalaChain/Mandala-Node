0172d868f0cc0c573ce82661826b22d3296482f3
Agustinus Theodorus <agustinustheoo@gmail.com>
2024-09-28 00:27:33 +0700

    feat: cumulus storage proof size host functions

--- node/src/service.rs
+++ node/src/service.rs
@@ -58,14 +58,13 @@ pub struct ParachainNativeExecutor;
 
 #[cfg(feature = "runtime-benchmarks")]
 pub type HostFunctions = (
-    sp_io::SubstrateHostFunctions,
+    cumulus_client_service::ParachainHostFunctions,
     frame_benchmarking::benchmarking::HostFunctions,
     moonbeam_primitives_ext::moonbeam_ext::HostFunctions,
 );
 
 #[cfg(not(feature = "runtime-benchmarks"))]
 pub type HostFunctions = (
-    sp_io::SubstrateHostFunctions,
     cumulus_client_service::ParachainHostFunctions,
     moonbeam_primitives_ext::moonbeam_ext::HostFunctions,
 );
